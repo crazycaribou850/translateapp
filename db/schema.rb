1e64ba4051eb61952d6876dc9dd76be9340987ff
Tommy Poa <Tommy@Tommys-MacBook-Pro.local>
2018-04-22 16:25:20 -0700

    Added Language and Playlist model, got the languages#show method to work.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180420024108) do
+ActiveRecord::Schema.define(version: 20180422232004) do
+
+  create_table "languages", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "playlists", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "usercount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "language_id"
+    t.index ["language_id"], name: "index_playlists_on_language_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
